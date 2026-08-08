9d9955a6fc1b8d075a5e16371d2ab694657ba5c3
Mateus111155 <mateusfelipe.batista@hotmail.com>
2026-08-08 12:17:43 -0300

    Initialize variables and log their values
    Added variables for name, age, city, and points with console logs.

--- aula01.js
+++ aula01.js
@@ -1,1 +1,20 @@
+let nome = "Mateus";
+let idade = 18;
+let cidade = "São Paulo";
 
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+let pontos = 10;
+
+console.log(pontos);
+
+pontos = 20;
+
+console.log(pontos);
+10
+20
+
+const nome = "Mateus";
+
+console.log(nome);

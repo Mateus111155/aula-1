2117cb3f0c352b820beebca4fe606ec032a3d456
Mateus111155 <mateusfelipe.batista@hotmail.com>
2026-08-08 12:07:30 -0300

    Create aula02.js

---
+++ aula02.js
@@ -0,0 +1,41 @@
+let idade = 18;
+
+if (idade >= 18) {
+    console.log("Você é maior de idade!");
+} else {
+    console.log("Você é menor de idade!");
+}
+
+function dizerOla() {
+    console.log("Olá!");
+}
+
+dizerOla();
+function cumprimentar(nome) {
+    console.log("Olá, " + nome + "!");
+}
+
+cumprimentar("Mateus");
+
+let fotos = [
+    "foto01.jpg",
+    "foto02.jpg",
+    "foto03.jpg"
+];
+
+console.log(fotos[0]);
+console.log(fotos[1]);
+console.log(fotos[2]);
+
+let fotos = [
+    "casamento01.jpg",
+    "casamento02.jpg",
+  let fotos = [
+    "foto01.jpg",
+    "foto02.jpg",
+    "foto03.jpg"
+];
+
+for (let i = 0; i < fotos.length; i++) {
+    console.log(fotos[i]);
+}

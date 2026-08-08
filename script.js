d008c24bdcf36698883e92a3cca8d1247f821350
Mateus111155 <mateusfelipe.batista@hotmail.com>
2026-08-08 12:23:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ function selecionarFoto() {
 
     alert("Foto selecionada!");
 
-}
+let fotos = [
+    "https://picsum.photos/id/10/500/300",
+    "https://picsum.photos/id/20/500/300",
+    "https://picsum.photos/id/30/500/300"
+];
+
+let numero = 0;
+
 let foto = document.getElementById("foto");
 let botao = document.getElementById("botao");
 
 botao.addEventListener("click", function() {
 
-    foto.src = "https://picsum.photos/401/300";
+    numero++;
+
+    if (numero >= fotos.length) {
+        numero = 0;
+    }
+
+    foto.src = fotos[numero];
 
 });
-document.getElementById("foto");
